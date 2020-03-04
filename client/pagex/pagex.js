e6e641c3215f23e183d2ed9a7c3839aeb4dbea78
dkz <dkz@gmail.com>
2020-03-04 15:27:51 +0700

    dkz

---
+++ client/pagex/pagex.js
@@ -0,0 +1,168 @@
+const assert = require('assert')
+import './pagex.html';
+
+const TP = Template.pagex;
+
+const article = new ReactiveVar();
+const index_status = new ReactiveVar();
+
+
+TP.onCreated(function(){
+})
+
+TP.onRendered(function(){
+  const xid = this.data.xid();
+  assert(xid)
+
+  Meteor.call('get-itemx', {xid}, (err, data)=>{
+    if (err) throw err;
+    if (data.error) {
+      console.log(`get-article:`, data);
+      return;
+    }
+
+    const {data:a1} = data;
+    console.log(`@23: article `,a1)
+    article.set(a1)
+  }) // call
+
+})
+
+TP.helpers({
+  it: ()=>{
+    const a1 = article.get();
+//    console.log(`@36: helper it:`,a1)
+    return a1;
+  },
+  part(x) {
+    if (x>0) {
+      return `part:${x+1} - `;
+    }
+  },
+  raw_data() {
+    const o = app.article.get();
+    return o && JSON.stringify(o);
+  }
+
+});
+
+TP.events({
+  'click .js-save-selection':(e,tp)=>{
+    e.stopImmediatePropagation();
+    console.log(`.js-save-selection e:`,e);
+    /*
+        Collect all checked auteurs.
+    */
+    const v = tp.findAll('.js-select-auteur')
+    .filter(it=>(it.checked)).map(it=>it.value);
+    console.log(v)
+    Session.set('selected-auteurs',v)
+  }
+});
+
+
+TP.events({
+  'input .js-select-auteur': (e,tp)=>{
+    e.stopImmediatePropagation();
+    console.log(`input target (${e.target.name})=>(${e.target.value}) checked:`,e.target.checked);
+  },
+  'input': (e,tp)=>{
+    const value = e.target.value;
+    const etime = new Date().getTime();
+    console.log(`input2 target (${e.target.name})=>(${e.target.value})`);
+
+    const _value = value.toLowerCase()
+    .replace(/\s+/g,'\\s+')
+    .replace(/[aàáâä]/g,'[aàáâä]')
+    .replace(/[eèéêë]/g,'[eèéêë]')
+    .replace(/[iìíîï]/g,'[iìíîï]')
+    .replace(/[oòóôö]/g,'[oòóôö]')
+
+    // ------------------------------------------
+    if (!value || value.length<=2) { // reset to visible.
+      console.log('reset to visible')
+      for (j in auteurs) {
+        if (auteurs[j].dirty || (auteurs[j].hidden == true)) {
+          // restore
+          auteurs[j].hidden = false;
+//          auteurs[j].display_type = 'table-row';
+          const y = Object.assign({},auteurs[j]);
+          app.auteurs_array.set(j,y)
+          auteurs[j].dirty = false;
+        }
+      }
+      return;
+    }
+    // ------------------------------------------
+
+
+    console.log('_value:',_value)
+    const re = new RegExp(_value,'i')
+    console.log('re:',re)
+    for (j in auteurs) {
+      const name1 = auteurs[j].name;
+//      console.log(name1)
+
+      if (!name1.match(re)) {
+//        auteurs[j].hidden = true;
+        if (auteurs[j].dirty || (auteurs[j].hidden != true)) {
+          // restore
+//          auteurs[j].display_type = 'none';
+          auteurs[j].hidden = true;
+          const y = Object.assign({},auteurs[j]);
+          app.auteurs_array.set(j,y)
+          auteurs[j].dirty = false;
+        }
+        continue;
+      }
+
+
+//      const new_name = name1.replace(re,`<b style="color:blue;">${value}</b>`);
+      const new_name = name1.replace(re, (x,y,$3,$4)=>{
+        console.log(`replace x:(${x}) y:(${y})(${$3})(${$4})`)
+        return `<b style="color:blue;">${x}</b>`;
+      });
+
+//      console.log(new_name);
+      if (new_name == name1) {
+        // unchanged from the original
+//        auteurs[j].display_type = 'none';
+        auteurs[j].hidden = true;
+        /*
+        if (auteurs[j].dirty) {
+          // restore
+          const y = Object.assign({},auteurs[j]);
+          auteurs_array.set(j,y)
+          auteurs[j].dirty = false;
+        }
+        */
+      }
+      else {
+  //      auteurs[j].hidden = false;
+//        auteurs[j].display_type = 'table-row';
+        auteurs[j].hidden = false;
+        const y = Object.assign({},auteurs[j]);
+        y.name = new_name;
+        auteurs_array.set(j,y)
+        auteurs[j].dirty = true;
+      }
+    } // loop
+    console.log(`etime to update: ${new Date().getTime()-etime}ms.`)
+  } // input
+
+})
+
+
+
+// ============================================================================
+
+FlowRouter.route('/pagex/:xid', {
+  name: 'article',
+    action: function(params, queryParams){
+        console.log('Router::action for: ', FlowRouter.getRouteName());
+        console.log(' --- params:',params);
+//        document.auteur = "Museum v9";
+//        app.article_id.set(undefined);
+        BlazeLayout.render('pagex', params);
+    }
+});
